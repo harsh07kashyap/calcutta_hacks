ec71e2b7bb0d669e2653bb0087d63f0b4d87e9df
harsh07kashyap <148455873+harsh07kashyap@users.noreply.github.com>
2025-12-28 08:07:24 +0530

    backend url updated

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000";
+const API_URL = "https://calcutta-hacks.onrender.com";
 let currentUser = null;
 let currentReviewPlaceId = null;
 
@@ -237,4 +237,5 @@ async function fetchWeather(lat, lng) {
     } catch (error) {
         console.error("Weather Error:", error);
     }
+
 }
